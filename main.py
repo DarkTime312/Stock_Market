1da185ac53026a70e9b8587dfda8e76fd61ea8a5
rozes <roze.sorkh.777@gmail.com>
2024-07-07 04:31:12 +0330

    0.3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
 import yfinance as yf
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from zoneinfo import ZoneInfo
 
 from datetime import datetime, timedelta, timezone
 from settings import *
@@ -11,6 +11,11 @@ try:
     from ctypes import windll, byref, sizeof, c_int
 except:
     pass
+
+
+def get_ny_date():
+    ny_time = datetime.now(ZoneInfo("America/New_York"))
+    return ny_time.date()
 
 
 class StockMarket(ctk.CTk):
@@ -23,8 +28,12 @@ class StockMarket(ctk.CTk):
         self.iconbitmap('empty.ico')
         self.change_titlebar_color()
 
-        StockFigure(self)
-        BottomFrame(self)
+        # variables
+        symbol_string = ctk.StringVar()
+        period_string = ctk.StringVar(value='Max')
+
+        StockFigure(self, symbol_string, period_string)
+        BottomFrame(self, symbol_string, period_string)
 
     def change_titlebar_color(self):
         try:
@@ -37,25 +46,48 @@ class StockMarket(ctk.CTk):
 
 
 class StockFigure(ctk.CTkFrame):
-    def __init__(self, parent):
-        super().__init__(master=parent)
+    def __init__(self, parent, symbol_string, period_string):
+        super().__init__(master=parent, fg_color=BG_COLOR)
         self.pack(pady=10, expand=True, fill="both")
+        self.symbol_string = symbol_string
+        self.period_string = period_string
 
-        self.fetch_and_plot()
+        symbol_string.trace('w', self.fetch_and_plot)
+        period_string.trace('w', self.fetch_and_plot)
 
-    def fetch_and_plot(self):
-        # Set the stock symbol
-        symbol = 'AAPL'
+        # self.fetch_and_plot()
+
+    def fetch_and_plot(self, *args):
+        # Get the stock symbol from user
+        symbol = self.symbol_string.get().upper()
+        if not symbol:
+            return
 
         # Fetch data for the last week
-        end_date = datetime.now(timezone.utc)
-        start_date = end_date - timedelta(days=8)
 
         # Create a Ticker object for the specified symbol
         stock = yf.Ticker(symbol)
-        # data = stock.history(period='1y')
+
+        match self.period_string.get():
+            case 'Max':
+                data = stock.history(period='max')
+            case 'Week':
+                end_date = get_ny_date()
+                start_date = end_date - timedelta(days=8)
+                data = stock.history(start=start_date, end=end_date)
+            case '1 Year':
+                data = stock.history(period='1y')
+
+            case 'Month':
+                data = stock.history(period='1mo')
+
+            case '6 Months':
+                data = stock.history(period='6mo')
+
+            case _:
+                return
+
         # Fetch historical data for the specified date range
-        data = stock.history(start=start_date, end=end_date)
 
         # Check if the data is empty
         if data.empty:
@@ -103,9 +135,47 @@ class StockFigure(ctk.CTkFrame):
 
 
 class BottomFrame(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, symbol_string, period_string):
         super().__init__(master=parent, fg_color=INPUT_BG_COLOR, height=50, corner_radius=0)
         self.pack(side="bottom", fill="x")
+        self.symbol_string = symbol_string
+        self.period_string = period_string
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.entry = ctk.CTkEntry(self, )
+        self.entry.insert(0, 'AAPL')
+        self.entry.pack(side='left', pady=10, padx=10)
+        self.entry.bind('<Return>', self.draw_the_figure)
+        self.list = []
+
+        for time in TIME_OPTIONS:
+            self.list.append(LabelButton(self, time, self.list, self.period_string))
+
+    def draw_the_figure(self, event):
+        self.symbol_string.set(self.entry.get())
+
+
+class LabelButton(ctk.CTkLabel):
+    def __init__(self, parent, text, list, period_string, text_color=TEXT_COLOR):
+        super().__init__(master=parent, text=text, text_color=text_color)
+        self.list = list
+        self.period_string = period_string
+        self.pack(side='right', padx=10)
+        self.bind('<Button-1>', lambda e, x=text: self.set_period(period=x))
+        self.set_period('Max')
+
+    def set_period(self, period):
+        self.set_active(period)
+
+    def set_active(self, period):
+        for widget in self.list:
+            if widget.cget('text') == period:
+                widget.configure(text_color=HIGHLIGHT_COLOR)
+            else:
+                widget.configure(text_color='white')
+        self.period_string.set(period)
 
 
 if __name__ == '__main__':
